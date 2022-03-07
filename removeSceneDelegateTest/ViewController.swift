fbfe790487b746ebc2e030776f9e76e60ab9d10f
Neo <yenhachi8@gmail.com>
2022-03-07 11:47:28 +0800

    Initial Commit

---
+++ removeSceneDelegateTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  removeSceneDelegateTest
+//
+//  Created by DGAdministrator on 2022/3/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
